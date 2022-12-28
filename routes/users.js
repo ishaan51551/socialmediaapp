5cee4b30f3dc7b55f538ab36604452fa42d16b1b
ISHAAN SAHU <98735376+ishaan51551@users.noreply.github.com>
2022-12-28 21:23:00 +0530

    Delete routes directory

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-import Express, { Router } from "express";
-import { getUser,updateUser } from "../controllers/user.js";
-
-const router=Express.Router();
-
-router.get("/find/:userId",getUser);
-router.put("/",updateUser);
-
-export default router;
